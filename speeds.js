cb3d98b987e6d3e260d7345508f3a8b278464f5a
Ethan <116187182+Cubeacabra@users.noreply.github.com>
2026-02-17 14:20:06 -0800

    Update speeds.js

--- speeds.js
+++ speeds.js
@@ -31,7 +31,8 @@ speedUpButton.addEventListener('click', speeed);
 slowDownButton.addEventListener('click', slooow);
 
 
-document.querySelector("body").addEventListener("mouseleave", (_) => {
+/*document.querySelector("body").addEventListener("mouseleave", (_) => {
   alert("YOU CANNOT ESCAPE THE LOBSTERS");
   window.open("https://bluelobster.netlify.app/", "_blank").focus();
 });
+*/
